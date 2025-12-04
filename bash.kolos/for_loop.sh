dd75ce43dc45d343de82850074d6f880e40095b7
ligusoliwia <ligus.oliwiaa@gmail.com>
2025-12-04 19:53:17 +0100

    committed

---
+++ bash.kolos/for_loop.sh
@@ -0,0 +1,11 @@
+#/!usr/bin/bash
+
+#pętla for
+#przechodzi po wartościach których ilość jest znana
+
+for ((i = 0; i < 10; i++)); do
+#budowa pętli for:
+    #for ((zmienna; warunek kończący pętlę; instrukcja: +/- zmienna)); do
+    #np.: ((i = 0; i < n; i++)) == ((i = 0; i < n; i = i + 1))
+echo $i #wyświetla zmienną po każdej iteracji
+done
