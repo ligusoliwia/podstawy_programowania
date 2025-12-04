40cd68e728bd4f862a577b7b22b884b52b0e654f
ligusoliwia <ligus.oliwiaa@gmail.com>
2025-12-04 20:08:02 +0100

    committed

--- bash.kolos/while_loop.sh
+++ bash.kolos/while_loop.sh
@@ -5,7 +5,8 @@
 
 declare -i i=0 #deklaracja zmiennej 'i'
 while ((i<10)); do 
-#postawienie warunku, dopółki i<10 kod będzie powtarzany
-echo $i #za każdą iteracją zmienna 'i' będzie wyświetlana
-((i = i + 1)) #jeśli warunek pętli nie jest spełniony -> instrukcja
+    #postawienie warunku, dopółki i < 10 kod będzie powtarzany
+    echo $i #za każdą iteracją zmienna 'i' będzie wyświetlana
+    ((i = i + 1)) #jeśli warunek pętli nie jest spełniony -> instrukcja
+    #między 'while' a 'done' są polecenia do wykonania w tej pętli
 done
