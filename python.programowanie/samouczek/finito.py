f37dc4c64c7f94bce6b0563dd5983ebb17451eec
ligusoliwia <ligus.oliwiaa@gmail.com>
2025-12-18 11:04:43 +0100

    k

---
+++ python.programowanie/samouczek/finito.py
@@ -0,0 +1,9 @@
+#Napisz program, który będzie wczytywał od użytkownika wyrazy (ciągi znaków), 
+#dopóki użytkownik nie wpisze słowa „finito”. 
+#Wczytane wyrazy mają być umieszczone na liście.
+
+lista = input("proszę ładnie o wprowadzenie liczby: ")
+    if lista == "finito":
+        break
+    else continue
+print(lista)
