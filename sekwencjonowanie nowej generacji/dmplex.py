31ac903554b76f7ea35d7954ce55b04ddf166dfc
ligusoliwia <ligus.oliwiaa@gmail.com>
2026-03-15 10:25:00 +0100

    k

---
+++ sekwencjonowanie nowej generacji/dmplex.py
@@ -0,0 +1,49 @@
+#czytanie indexow i slownik
+def read_id(indexy_plik):
+    index_dict = {}
+
+    with open(indexy_plik, "r") as inx:
+        for line in inx:
+            separated = line.strip().split("\t") #dzieli po tabulatorze
+            if len(separated) >= 3: #sample_id, start, stop
+                sample_name = separated[0].strip() #nazwa probki
+                index_1 = separated[1].strip() #index poczatkowy (i1)
+                index_2 = separated[2].strip() #index koncowy (i2)
+                inx = f"{index_1}+{index_2}"
+                index_dict[inx] = sample_name #format zapisania indexow
+                               
+    return index_dict
+
+#sprawdzanie indexow z seqwncjami
+def dmplex(seq_plik, indexy):
+    #nazwa_probki -> lista pełnych sekwencji
+    results = {}
+    
+    results["bleh"] = [] #na sekwencje niedopasowane
+
+    #listy dla kazdej probki
+    for sample in indexy.values():
+        results[sample] = [] #'sample_id:" "jakas sekwencja", tworzy liste od nazw sampli w dict
+
+    with open(seq_plik, "r") as seq:
+        for line in seq:
+            line = line.strip()
+
+            #snipsnip indeksy (8 od początku i od końca)
+            i1 = line[:8] #poczatkowy barcode
+            i2 = line[-8:] #koncowy barcode
+            key = f"{i1}+{i2}"
+            
+            #czy i1+i2 sekwencji pasuja do jakiegos sampla z dict??
+            sample_id = indexy.get(key, "bleh")
+                #.get() → jesli znajdzie index to dodaje do listy danego sampla, jesli nie (alt) dodaje to bleh
+            
+            #przypisanie sekwencji do sampla
+            results[sample_id].append(line)
+            
+    return results
+
+
+index = read_id("indexy.txt")
+sorted_results = dmplex("sekwencje.txt", index)
+print(sorted_results)
