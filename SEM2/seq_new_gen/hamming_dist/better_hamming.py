7fb4946867f69370d2ccfe1b9125505159783c8e
ligusoliwia <ligus.oliwiaa@gmail.com>
2026-03-29 20:06:41 +0200

    k

---
+++ SEM2/seq_new_gen/hamming_dist/better_hamming.py
@@ -0,0 +1,82 @@
+def hamming(seq1, seq2):
+    distance = 0
+    for i in range(len(seq1)):
+        if seq1[i] != seq2[i]:
+            distance += 1
+    return distance
+
+def res_record(file, distance, seq1, seq2):
+    with open(file, "a") as res:
+        res.write(f"para {seq1} i {seq2} ma długość {distance}" + "\n")
+
+bugs = {1: "różna długość porównywanych sekwencji",
+        2: "niezidentyfikowany znak",
+        }
+
+def bug_log(file, bug_type, seq1, seq2):
+    bug_type = int(bug_type)
+    with open(file, "a") as bug:
+        bug.write(f"BŁĄD! para {seq1} i {seq2}, typ: {bugs[bug_type]}\n")
+
+
+
+def ham_res(file, compare):
+    allowed = set("ACTG")
+    with open(file, "r") as sq:
+        sq = sq.readlines()
+        seq = [line.strip() for line in sq]
+
+    if len(seq) % 2 != 0 and compare == 2:
+        compare = 1
+        print("liczba sekwencji w pliku jest nieparzysta, zmiana trybu porównywania na 1")
+
+    if compare == 1:
+        results = []
+        #petla zew: wybiera 1 seq
+        for i in range(len(seq)):
+            #petla wew: wybiera kolejna seq
+            for j in range(i + 1, len(seq)):
+                s1 = seq[i]
+                s2 = seq[j]
+
+                #zgodnosc znakow
+                if not (set(s1).issubset(allowed) and set(s2).issubset(allowed)):
+                    bug_log("bug_logger_hamming.txt", 2, s1, s2)
+                    continue
+
+                if len(s1) == len(s2):
+                    distance = hamming(s1, s2)
+                    res_record("hamming_results.txt", distance, s1, s2)
+                else:
+                    bug_log("bug_logger_hamming.txt", 1, s1, s2)
+                    print(f"wystąpił błąd, para {s1} i {s2}; patrz plik z błędami")
+    
+    if compare == 2:
+        results = []
+        for i in range(0, len(seq) - 1, 2): #range od zera do dlugosc pliku - 1, skok o 2
+            s1, s2 = seq[i], seq[i+1] #dwie linie
+            #sprawdzanie zgodnosci znakow
+            if not (set(s1).issubset(allowed) and set(s2).issubset(allowed)):
+                bug_log("bug_logger_hamming.txt", 2, s1, s2)
+                continue
+
+            if len(s1) == len(s2):
+                distance = hamming(s1, s2)
+                res_record("hamming_results.txt", distance, s1, s2)
+            else:
+                bug_log("bug_logger_hamming.txt", 1, s1, s2)
+                print(f"wystąpił błąd, para {s1} i {s2}; patrz plik z błędami")
+    
+
+
+
+comp = input('''jakie porowananie sekwencji (podaj numer):
+             1. każda z każdą,
+             2. pierwsza z następną.
+             wybór: ''')
+c_type = int(comp)
+
+if c_type not in [1, 2]:
+    print("niepoprawny tryb, spróbuj ponownie :)")
+
+ham_res("testSEQ.txt", c_type)
--- SEM2/seq_new_gen/hamming_dist/hamming_low.py
+++ SEM2/seq_new_gen/hamming_dist/hamming_low.py
@@ -1,13 +1,3 @@
-with open("testSEQ.txt", "r") as sq:
-    seq = sq.readlines()
-    for line in seq:
-        line.strip()
-print(seq)
-
-
-
-
-
 seq1 = "ACTGGT" #["A", "C", "T", "G", "G", "T"]
 seq2 = "AATGCA" #["A", "A", "T", "G", "C", "A"]
 
@@ -17,7 +7,7 @@ for i in range(len(seq1)):
         if seq1[i] == seq2[i]:
             hamming.append(0)
         else:
-            hamming.append(1) #jadna zmienna do sum(hamm)
+            hamming.append(1)
     else:
         break
 
